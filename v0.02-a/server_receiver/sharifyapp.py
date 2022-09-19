836a0129e315cde4fdb2c23a3a6b6be37f0fc408
Ali <ali.khelfaoui@icloud.com>
2022-09-19 22:34:56 +0200

    Réception du png fonctionnelle. Nouvelle architecture server/client.

--- v0.02-a/server_receiver/sharifyapp.py
+++ v0.02-a/server_receiver/sharifyapp.py
@@ -1,8 +1,8 @@
-# SERVER_RECEIVER
+# DISPLAY_RECEIVER
 
 # Variables 
 connected = False
-code_ok = False
+code_ok = True
 code_ko = False
 code_waiting = True # Ajouter condition si codes précédemment enregistrés, alors False
 running = True
@@ -14,47 +14,6 @@ SEPARATOR = "<SEPARATOR>"
 # Écran
 screen_width = 640
 screen_height = 160
-
-# Fonctions
-def connexion(): # Ajouter documentation
-    sharify_connect_receiver = socket.socket()
-    sharify_connect_receiver.setblocking(0)
-    sharify_connect_receiver.bind((SERVER_HOST, SERVER_PORT))
-    sharify_connect_receiver.listen(10)
-    logging.info(f"En attente... ({SERVER_HOST} sur le port {SERVER_PORT})")
-    while True: # Boucle sans conditions
-        try:
-            socket_sender, address = sharify_connect_receiver.accept()
-            logging.info(f"Connecté à {address}.")
-            connected = True
-            break
-        except BlockingIOError: # Si cette erreur est détectée, alors ...
-            logging.error("Erreur.")
-            socket_sender = 0
-            address = 0
-            connected = False
-            break
-    return sharify_connect_receiver, socket_sender, connected
-
-def reception_png(): # Ajouter documentation
-    # Échange de métadonnées
-    received_metadata = socket_sender.recv(BUFFER_SIZE).decode() # Précédente erreur due à une confusion entre sockets (socket_sender et non pas sharify_...)
-    filename, filesize = received_metadata.split(SEPARATOR)
-    filename = os.path.basename(filename) # Supprime le chemin d'accès du nom du fichier reçu
-    filesize = int(filesize)
-    # Réception du fichier
-    progress = tqdm.tqdm(range(filesize), f"Réception de {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename, "wb") as spotifycode:
-        while True:
-            bytes_read = socket_sender.recv(BUFFER_SIZE) # Précédente erreur due à une confusion entre sockets (socket_sender et non pas sharify_...)
-            if not bytes_read:
-                break
-            spotifycode.write(bytes_read)
-            progress.update(len(bytes_read))
-            socket_sender.close()
-            sharify_connect_receiver.close()
-    # Ajouter condition si code_ko (erreur)
-    # Ajouter condition si code_waiting (en attente de connexion ET aucun code précédemment enregistré)
 
 # Chargement et initialisation des libraries
 import pygame
@@ -73,35 +32,22 @@ logging.info("Date : 18SEP2022")
 # Initialisation écran
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Temporaire : connexion (en attendant de faire fonctionner la fonction connexion())
-# sharify_connect_receiver = socket.socket()
-# sharify_connect_receiver.bind((SERVER_HOST, SERVER_PORT))
-# sharify_connect_receiver.listen(10)
-# logging.info(f"En attente... ({SERVER_HOST} sur le port {SERVER_PORT})")
-# logging.info(f"Connecté à {address}.")
-# connected = True
-# reception_png()
-# code_ok = True
-
 # Boucle principale
 while running:
     if connected == False:
         spotifybar = pygame.image.load("Spotify_Code_waiting.png")
         screen.blit(spotifybar, (0, 0))
         pygame.display.flip()
-        connexion()
-    if connected == True:
-        # Affichage du Code Spotify
-        if code_ok:
-            spotifybar = pygame.image.load("Spotify_Code.png")
-            screen.blit(spotifybar, (0,0))
-            pygame.display.flip()
-        elif code_ko:
-            spotifybar = pygame.image.load("Spotify_Code_ko.png")
-            screen.blit(spotifybar, (0,0))
-            pygame.display.flip()
+    # if connected == True:
+    #     # Affichage du Code Spotify
+    #     if code_ok:
+    #         spotifybar = pygame.image.load("Spotify_Code.png")
+    #         screen.blit(spotifybar, (0,0))
+    #         pygame.display.flip()
     for event in pygame.event.get():
-        # screen.blit(spotifybar, (0, 0))        
-        # pygame.display.flip()
+        os.system("python3 server.py")
+        spotifybar = pygame.image.load("Spotify_Code.png")
+        screen.blit(spotifybar, (0, 0))        
+        pygame.display.flip()
         if event.type == pygame.QUIT:
             running = False
