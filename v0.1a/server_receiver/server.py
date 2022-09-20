3142ff134a094237b5a0ad3805b930e26fe65d8b
Ali <ali.khelfaoui@icloud.com>
2022-09-20 20:48:04 +0200

    try/except sur server.py

--- v0.1a/server_receiver/server.py
+++ v0.1a/server_receiver/server.py
@@ -14,22 +14,29 @@ SEPARATOR = "<SEPARATOR>"
 # Gestion du socket
 receiver = socket.socket()
 receiver.bind((SERVER_HOST, SERVER_PORT))
+receiver.setblocking(0)
 receiver.listen(1)
 print(f"En attente sur {SERVER_HOST}:{SERVER_PORT}.")
-sender, address = receiver.accept()
-print("Connexion acceptée.")
-# Réception
-received_mdata = sender.recv(BUFFER_SIZE).decode()
-filename, filesize = received_mdata.split(SEPARATOR)
-filename = os.path.basename(filename)
-filesize = int(filesize)
-progress = tqdm.tqdm(range(filesize), f"Réception du fichier... {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "wb") as spotifycode:
-    while True: # Essayer plus tard une structure try/except
-        bytes_read = sender.recv(BUFFER_SIZE)
-        if not bytes_read:
-            break
-        spotifycode.write(bytes_read)
-        progress.update(len(bytes_read))
-sender.close()
-receiver.close()
+while True:
+    try:
+        sender, address = receiver.accept()
+        print("Connexion acceptée.")
+        # Réception
+        received_mdata = sender.recv(BUFFER_SIZE).decode()
+        filename, filesize = received_mdata.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        progress = tqdm.tqdm(range(filesize), f"Réception du fichier... {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as spotifycode:
+            while True: # Essayer plus tard une structure try/except
+                bytes_read = sender.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                spotifycode.write(bytes_read)
+                progress.update(len(bytes_read))
+        sender.close()
+        receiver.close()
+        break
+    except BlockingIOError:
+       print("Client non trouvé.")
+       pass
