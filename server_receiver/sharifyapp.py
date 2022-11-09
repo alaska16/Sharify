3e40a2921b30235ecd9c857719a888af8800a34a
alaska16 <ali.khelfaoui@icloud.com>
2022-11-09 02:18:28 +0100

    Réagencement des fichiers.

--- server_receiver/sharifyapp.py
+++ server_receiver/sharifyapp.py
@@ -9,20 +9,23 @@ def interface_attente():
     pygame.draw.line(screen, r, (0, 160), (640, 160))
     attente = font.render("En attente...", True, w)
     screen.blit(attente, (8, 200))
-def interface(titre_musique, titre_album, artiste): # Paramètres : variable titre musique, artiste, AJOUTER GESTION DE L'ECRAN TACTILE ET LES MENUS
-    albumcover = pygame.image.load("placeholder.png")
-    pygame.draw.line(screen, g, (0, 160), (640, 160))
+def interface(titre_musique, titre_album, artiste, message): # Paramètres : variable titre musique, artiste, AJOUTER GESTION DE L'ECRAN TACTILE ET LES MENUS
+    albumcover = pygame.image.load("placeholder128.png")
+    pygame.draw.line(screen, gspotify, (0, 160), (640, 160))
     titrem_interface = font.render(titre_musique, True, w)
     titrea_interface = font.render(titre_album, True, w)
     artiste_interface = font.render(artiste, True, w)
+    message = font.render(message, True, gspotify)
     screen.blit(titrem_interface, (200, 200))
     screen.blit(titrea_interface, (200, 250))
     screen.blit(artiste_interface, (200, 300))
     screen.blit(albumcover, (8, 200))
+    screen.blit(message, (200, 350))
 
 # Variables 
 connected = False
 running = True
+message = "Message personnalisé"
 #titre_musique = "0"
 #titre_album = "1"
 #artiste = "2"
@@ -39,6 +42,7 @@ screen_width = 640
 screen_height = 400
 r = (255, 0, 0)
 g = (0, 255, 0)
+gspotify = (30, 215, 96)
 b = (0, 0, 255)
 w = (255, 255, 255)
 
@@ -56,7 +60,7 @@ pygame.init()
 logging.info("Chargement et initialisation des librairies terminée.")
 print("Libraries chargées et initialisées.")
 font = pygame.font.Font(None, 32)
-pygame.display.set_caption("Sharify v0.1.1a")
+pygame.display.set_caption("Sharify v0.2a")
 logging.basicConfig(filename='sharifyapp.log', encoding='utf-8', level=logging.DEBUG)
 
 # Lancement du serveur
@@ -76,7 +80,7 @@ while running:
         try:
             spotifybar = pygame.image.load("Spotify_Code.png")
             screen.blit(spotifybar, (0, 0))
-            interface("Titre morceau", "Titre album", "Nom de l'artiste")        
+            interface("Titre morceau", "Titre album", "Nom de l'artiste", message)        
             pygame.display.flip()
         except FileNotFoundError:
             spotifybar = pygame.image.load("Spotify_Code_empty.png")
@@ -84,4 +88,5 @@ while running:
             interface_attente()        
             pygame.display.flip()
         if event.type == pygame.QUIT:
+            # Pour plus tard : stopper le subprocess
             running = False
